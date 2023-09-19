a68e54eeba4ed5f24e790ab717ddebd97dc1f43f
brijesh11287 <142497033+brijesh11287@users.noreply.github.com>
2023-09-19 10:41:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# leetCode
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-jg8mkg)
---
+++ index.js
@@ -0,0 +1,6 @@
+const nums = [2, 7, 11, 15];
+let target = 9;
+
+const twoSum = (nums, target)=>{
+  
+}
