d88bdda5cb179e559390204469f877f1de93d880
brijesh11287 <142497033+brijesh11287@users.noreply.github.com>
2023-09-20 01:07:44 +0530

    final

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const nums = [2, 7, 11, 15];
-let target = 9;
+const nums = [5, 0, 5, 5];
+let target = 10;
+const twoSum = (nums, target) => {
+  const data = [];
+  for (let i = 0; i < nums.length; i++) {
+    let difference = target - nums[i];
+    let indexData = nums.indexOf(difference);
+    if (indexData !== i && indexData !== -1) {
+      data.push(indexData, i);
+      return data;
+    }
+  }
+};
 
-const twoSum = (nums, target)=>{
-  
-}
+console.log(twoSum(nums, target));
