39aef5d1048bbe9e242efda99ffea81adead2893
Brijesh Gupta <brijeshngi@gmail.com>
2023-09-23 12:25:03 +0530

    final

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-const roman = {
-  I: 1,
-  V: 5,
-  X: 10,
-  L: 50,
-  C: 100,
-  D: 500,
-  M: 1000,
-};
-
-function Roman(s) {
-  let number = 0;
-  for (let i = s.length - 1; i >= 0; i--) {
-    if (roman[s[i]] < roman[s[i + 1]]) {
-      number = number - roman[s[i]];
-    } else {
-      number = number + roman[s[i]];
-    }
-  }
-  return number;
+const str = ["flower", "flow", "flight"];
+const longestPrefix = [];
+for (let i = 0; i < str[0].length; i++) {
+  // console.log(str[0][i]);
+  console.log(str[i] === str[0][i]);
 }
-console.log(Roman("IV"));
