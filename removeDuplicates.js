ff5f4370eb698770c406271bde7ede95c0a5f209
Brijesh Gupta <brijeshngi@gmail.com>
2023-09-25 15:47:55 +0530

    updated

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,5 +1,13 @@
-const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
-
-const data = Set(nums);
-
-console.log(data);
+const nums = [1, 1, 2];
+for (i = 0; i < nums.length; i++) {
+  for (j = i + 1; j < nums.length; j++) {
+    if (nums[i] === nums[j]) {
+      nums[j] = "_";
+    } else {
+    }
+  }
+}
+const data = nums.filter((blank) => {
+  return blank !== "_";
+});
+console.log(data.length);
