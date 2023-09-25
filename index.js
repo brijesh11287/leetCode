3e929e847f0ccc853661654cc0c8984ba71945a2
Brijesh Gupta <brijeshngi@gmail.com>
2023-09-25 14:45:42 +0530

    final

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-// balanced parenthethesis
+var removeDuplicates = function (nums) {
+  const data = new Set(nums);
+  return data;
+};
 
-const paranthesis = ["()", "[]", "{}"];
-let str = "{[()]}";
-const arr = [];
-let i = 0;
-while (i < str.length) {
-  arr.push(str[i]);
-  i++;
-  let open = arr[arr.length - 2];
-  let closed = arr[arr.length - 1];
-  console.log(open);
-  console.log(closed);
-  let validParenthesis = open + closed;
-  if (paranthesis.includes(validParenthesis)) {
-    arr.pop();
-    arr.pop();
-  }
-}
-console.log(arr.length === 0);
+console.log(removeDuplicates([1, 1, 2]));
---
+++ removeDuplicates.js
@@ -0,0 +1,5 @@
+const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+
+const data = Set(nums);
+
+console.log(data);
