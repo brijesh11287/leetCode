9e393ebcb9c10e5ae5dd6f83b123ef310feb70ba
Brijesh Gupta <brijeshngi@gmail.com>
2023-09-25 20:26:26 +0530

    final

--- searchInsertPosition.js
+++ searchInsertPosition.js
@@ -1,13 +1,15 @@
 const nums = [3, 5, 7, 9, 10];
 let target = 8;
-
-if (nums.includes(target)) {
-  let indexOfTarget = nums.indexOf(5);
-  console.log(indexOfTarget);
-} else {
-  nums.push(target);
-  const newNums = nums.sort((a, b) => a - b);
-  console.log(newNums);
-  let indexOfTarget = newNums.indexOf(target);
-  console.log(indexOfTarget);
+function searchInsertPosition(nums, target) {
+  if (nums.includes(target)) {
+    let indexOfTarget = nums.indexOf(5);
+    return indexOfTarget;
+  } else {
+    nums.push(target);
+    const newNums = nums.sort((a, b) => a - b);
+    let indexOfTarget = newNums.indexOf(target);
+    return indexOfTarget;
+  }
 }
+
+console.log(searchInsertPosition(nums, target));
