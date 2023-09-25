7cad7ef35bf644b27f703507a954e1a87a6b20cf
Brijesh Gupta <brijeshngi@gmail.com>
2023-09-25 10:45:04 +0530

    final

---
+++ index.js
@@ -0,0 +1,20 @@
+// balanced parenthethesis
+
+const paranthesis = ["()", "[]", "{}"];
+let str = "{[()]}";
+const arr = [];
+let i = 0;
+while (i < str.length) {
+  arr.push(str[i]);
+  i++;
+  let open = arr[arr.length - 2];
+  let closed = arr[arr.length - 1];
+  console.log(open);
+  console.log(closed);
+  let validParenthesis = open + closed;
+  if (paranthesis.includes(validParenthesis)) {
+    arr.pop();
+    arr.pop();
+  }
+}
+console.log(arr.length === 0);
